04765920ed1b4245e4ccf3d8e9311b3e31b84537
camilapramos <cpr.fisica@gmail.com>
2023-09-14 16:22:01 -0300

    added dmsimp model files

--- models/DMsimp_Spin1/object_library.py
+++ models/DMsimp_Spin1/object_library.py
@@ -172,13 +172,17 @@ all_CTparameters = []
 
 class CTParameter(UFOBaseClass):
 
-    require_args=['name', 'type', 'value', 'texname']
+    require_args=['name', 'nature,', 'type', 'value', 'texname']
 
     def __init__(self, name, type, value, texname):
 
-        args = (name,type,value,texname)
+        args = (name,'internal',type,value,texname)
 
         UFOBaseClass.__init__(self, *args)
+
+        args=(name,'internal',type,value,texname)
+
+        self.nature='interal'
 
         global all_CTparameters
         all_CTparameters.append(self)
